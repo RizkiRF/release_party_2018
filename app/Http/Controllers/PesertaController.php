a6aaab94899aae76ff33eb197c1888eda4bb634b
Diky Arga <dikyarga.id@gmail.com>
2016-04-15 22:20:41 +0700

    improve at after register

--- app/Http/Controllers/PesertaController.php
+++ app/Http/Controllers/PesertaController.php
@@ -45,7 +45,7 @@ class PesertaController extends Controller
 
         }
             
-        $totalharga = $jumlahpesertabayar * 50000;
+        $totalharga = $jumlahpesertabayar * 45000;
 
         return view('peserta.report')->withPesertas($pesertas)
                                      ->withJumlahpesertabayar($jumlahpesertabayar)
@@ -72,15 +72,15 @@ class PesertaController extends Controller
     public function store(Request $request)
     {
       // acak kode tiket, 5 kali repeat, di ambil 3 karakter dari index 0 sampai 3
-      $kode_tiket = substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyz", 5)), 0, 3);
+      $kode_tiket = substr(str_shuffle(str_repeat("123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", 5)), 0, 3);
 
       // kunci rahasia
-      $kunci_rahasia = substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyz", 5)), 0, 3);
+      $kunci_rahasia = substr(str_shuffle(str_repeat("123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", 5)), 0, 3);
       // cek apakah kode_tiket sudah pernah dipakai ?
       if(Peserta::where('kode_tiket', '=', $kode_tiket)->exists())
       {
         // jika pernah, maka acak lagi, sebenernya sih baiknya pake While, tapi aku rasa ini cukup untuk mengatisipasi sampai 1000 peserta
-        $kode_tiket = substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyz", 5)), 0, 3);
+        $kode_tiket = substr(str_shuffle(str_repeat("123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", 5)), 0, 3);
       }
 
       $peserta = new Peserta();
@@ -114,7 +114,7 @@ class PesertaController extends Controller
 
        //QrCode::generate($kode_tiket, '../public/qrcode.svg');
 
-      return view('terimakasih')->withNamapeserta($peserta->nama)->withEmail($peserta->email);
+      return view('terimakasih')->withNamapeserta($peserta->nama)->withEmail($peserta->email)->withKode_tiket($peserta->kode_tiket);
     }
 
     public function show_konfirmasi()
@@ -130,7 +130,7 @@ class PesertaController extends Controller
         // buar object pesertanya yang bakal di tambahin
         $peserta = Peserta::where('kode_tiket', '=', $kode_tiket)->firstOrFail();
           // acak kode tiket, 5 kali repeat, di ambil 3 karakter dari index 0 sampai 3
-        $penamaan_gambar = substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyz", 5)), 0, 3);
+        $penamaan_gambar = substr(str_shuffle(str_repeat("01123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", 5)), 0, 3);
 
         $peserta->atas_nama_pengirim = $request->atas_nama;
         $imageName = $penamaan_gambar. '.' . $request->file('image')->getClientOriginalExtension();
--- resources/views/terimakasih.blade.php
+++ resources/views/terimakasih.blade.php
@@ -13,23 +13,39 @@
 <div class="row">
     <div class="col-md-4 text-center">
         <a href="#"><img src="{{asset ('images/tealinux-line-icon.png')}}" alt="Logo Tealinux Line Icon"></a>
+        <h3>{{ $kode_tiket }}</h3>
+        <h6>Kode tiketmu</h6>
     </div>
     <div class="col-md-8">
         <h2>Terimakasih telah mendaftar, {{ $namapeserta }}</h2>
         <h5>Selanjutnya, segera lakukan pembayaran.</h5>
-        <p>Kamu dapat melakukan pembayaran dengan dua cara : <br>
-        <ul>
-            <li>Datang langsung ke stand / camp kami di Gedung D Lantai 1 Universitas Dian Nuswantoro</li>
+
+
+    </div>
+</div>
+    <div class="row">
+        <div class="col-md-12">
+            <h6>Kamu dapat melakukan pembayaran dengan dua cara :</h6> <br>
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <ul>
+                <li>Datang langsung ke stand / camp kami di Gedung D Lantai 1 Universitas Dian Nuswantoro</li>
+
+
+            </ul>
+        </div>
+        <div class="col-md-6">
             <li>Melakukan pendaftaran secara Online lalu melakukan pembayaran via rekening bank. (Nomer rekening dan prosedur lengkap telah kami kirim ke email anda : {{ $email }})</li>
             <ul>
                 <li>Lakukan pembayaran ke nomer rekening kami</li>
                 <li>Konfirmasi pembayaran mu ke <a href="#">halaman konfirmasi </a> / kirim sms dengan format "Nama Peserta Nama bank pengirim Atas nama pengirim" </li>
                 <li>Tunggu maximal 1 x 24 Jam, untuk kami melakukan pengecekan lalu kami akan menghubungimu ( :</li>
             </ul>
-        </ul>
-        </p>
+        </div>
     </div>
-</div>
 
 <br/>
 
