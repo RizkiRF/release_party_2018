46d90e79ee4468eab939553150d25b1dd558a1d2
Diky Arga <dikyarga.id@gmail.com>
2016-04-18 14:01:42 +0700

    data table and minor improvement

--- resources/views/peserta/all.blade.php
+++ resources/views/peserta/all.blade.php
@@ -6,6 +6,8 @@
             top: 0;
         }
     </style>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.11/css/dataTables.bootstrap.min.css"
+          xmlns="http://www.w3.org/1999/html">
 @endsection
 
 @section('title')
@@ -20,7 +22,7 @@ Table Peserta
                 <span class="glyphicon glyphicon-ok"></span> <em> {!! session('pesan') !!}</em></div>
         @endif
 
-        <table class="table table-striped " width="100%" cellspacing="0" >
+        <table id="example" class="table table-striped " width="100%" cellspacing="0" >
             <thead>
             <tr>
                 <th>ID</th>
@@ -37,28 +39,49 @@ Table Peserta
             </tr>
             </thead>
             <tbody>
+            <?php $no = 1 ?>
             @foreach($pesertas as $peserta)
                 <tr>
 
-                    <td>{{$peserta->id}}</td>
-                    <td>{{$peserta->nama}}</td>
+                    <td>{{$no}}</td>
+                    <td><a href="{{ url('/peserta/edit') . "/" . $peserta->id }}">{{$peserta->nama}}</a> </td>
                     <td>{{$peserta->no_hp}}</td>
                     <td>{{$peserta->email}}</td>
                     <td>{{$peserta->kode_tiket}}</td>
                     <td>{{$peserta->status}}</td>
                     <td>{{$peserta->dvd}}</td>
-                    <td>{{$peserta->status_bayar}}</td>
+                    <td>
+                    @if($peserta->status_bayar == 0)
+                        <span style="color:red;font-weight: bold;">Belum Lunas</span>
+                    @else
+                        <span style="color:limegreen;">Lunas</span>
+                    @endif
+                    </td>
                     <td>{{$peserta->email_terkirim}}</td>
                     <td>{{$peserta->sms_terkirim}}</td>
-                    <td>
+
                     <td><a href="{{ url('/peserta/delete/'.$peserta->id) }}">Delete</a>
                         <a href="{{ url('/peserta/edit/'.$peserta->id) }}">Edit</a></td>
 
                     </td>
                 </tr>
+                <?php $no++ ?>
             @endforeach
 
             </tbody>
         </table>
+            {{--{!! $pesertas->render() !!}--}}
     </div>
+@endsection
+
+
+@section('footer')
+    <script src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.11/js/dataTables.bootstrap.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable();
+        } );
+    </script>
+
 @endsection
