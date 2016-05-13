d035081db06df063a866d167ea5b96be40b6d1c4
Diky Arga <dikyarga.id@gmail.com>
2016-05-13 18:05:17 +0700

    tutup pendaftaran

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -356,63 +356,64 @@
     <div class="container" id="daftar">
         <div class="section-title">
             <h5>DAFTAR SEKARANG</h5>
-            <p>Jadi saksi perayaan di rilisnya TeaLinuxOS versi terbaru.</p>
+            {{--<p>Jadi saksi perayaan di rilisnya TeaLinuxOS versi terbaru.</p>--}}
+            <h6>Maaf, pendaftaran telah kami tutup, karena kuota peserta telah terpenuhi. Jika ada pertanyaan, silahkan hubungi kontak dibawah. Terimakasih (: </h6>
         </div>
-        <div class="contact-form bottom-space-xl wow fadeInUp">
-            <form action="{{ url('terimakasih')}}" id="" method="POST">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <div class="row">
-                    <div class="col-md-6 col-md-offset-3">
+        {{--<div class="contact-form bottom-space-xl wow fadeInUp">--}}
+            {{--<form action="{{ url('terimakasih')}}" id="" method="POST">--}}
+                {{--<input type="hidden" name="_token" value="{{ csrf_token() }}">--}}
+                {{--<div class="row">--}}
+                    {{--<div class="col-md-6 col-md-offset-3">--}}
 
-                        <div class="form-group">
-                            <label>Nama</label>
-                            <input type="text" class="form-control" name="nama" placeholder="Nama Lengkap Anda ( pastikan benar karena akan kami cetak di sertifikat )"  value="{{ old('nama') }}"required>
-                        </div>
-                        <div class="form-group">
-                            <label>Nomor HP</label>
-                            <input type="text" class="form-control" name="no_hp" value="{{ old('no_hp') }}" placeholder="Nomor HP yang dapat dihubungi" required>
-                        </div>
-                        <div class="form-group">
-                            <label>Email</label>
-                            <input type="email" class="form-control" name="email" value="{{old('email')}}" placeholder="Alamat Email" required>
-                        </div>
+                        {{--<div class="form-group">--}}
+                            {{--<label>Nama</label>--}}
+                            {{--<input type="text" class="form-control" name="nama" placeholder="Nama Lengkap Anda ( pastikan benar karena akan kami cetak di sertifikat )"  value="{{ old('nama') }}"required>--}}
+                        {{--</div>--}}
+                        {{--<div class="form-group">--}}
+                            {{--<label>Nomor HP</label>--}}
+                            {{--<input type="text" class="form-control" name="no_hp" value="{{ old('no_hp') }}" placeholder="Nomor HP yang dapat dihubungi" required>--}}
+                        {{--</div>--}}
+                        {{--<div class="form-group">--}}
+                            {{--<label>Email</label>--}}
+                            {{--<input type="email" class="form-control" name="email" value="{{old('email')}}" placeholder="Alamat Email" required>--}}
+                        {{--</div>--}}
 
-                        <div class="form-group has-info">
-                            <label class="">Status</label>
+                        {{--<div class="form-group has-info">--}}
+                            {{--<label class="">Status</label>--}}
 
-                            <select id="opsi-status" class="form-control" name="status_peserta" required="required">
-                                <option value="">-- Pilih --</option>
-                                <option value="mahasiswa">Mahasiswa</option>
-                                <option value="pelajar">Pelajar</option>
-                                <option value="umum">Umum</option>
-                            </select>
+                            {{--<select id="opsi-status" class="form-control" name="status_peserta" required="required">--}}
+                                {{--<option value="">-- Pilih --</option>--}}
+                                {{--<option value="mahasiswa">Mahasiswa</option>--}}
+                                {{--<option value="pelajar">Pelajar</option>--}}
+                                {{--<option value="umum">Umum</option>--}}
+                            {{--</select>--}}
 
 
-                        </div>
-                        <div class="instansi">
+                        {{--</div>--}}
+                        {{--<div class="instansi">--}}
 
-                        </div>
+                        {{--</div>--}}
 
-                        <div class="form-group has-info">
-                            <label class=""> DVD TeaLinux OS 8</label>
+                        {{--<div class="form-group has-info">--}}
+                            {{--<label class=""> DVD TeaLinux OS 8</label>--}}
 
-                            <select class="form-control" name="dvd" required="required">
-                                <option value="">--Pilih--</option>
-                                <option value="32">32 Bit</option>
-                                <option value="64">64 Bit</option>
+                            {{--<select class="form-control" name="dvd" required="required">--}}
+                                {{--<option value="">--Pilih--</option>--}}
+                                {{--<option value="32">32 Bit</option>--}}
+                                {{--<option value="64">64 Bit</option>--}}
 
-                            </select>
+                            {{--</select>--}}
 
-                        </div>
+                        {{--</div>--}}
 
-                        <div class="text-center top-space">
-                            <button type="submit" class="btn btn-success btn-block btn-lg" >Daftar</button>
+                        {{--<div class="text-center top-space">--}}
+                            {{--<button type="submit" class="btn btn-success btn-block btn-lg" >Daftar</button>--}}
 
-                        </div>
-                    </div>
-                </div>
-            </form>
-        </div>
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</form>--}}
+        {{--</div>--}}
     </div>
     <div class="highlight" id="kontak">
         <div class="container">
